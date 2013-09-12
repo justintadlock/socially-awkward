29bb78271dbb758f03970b28f6bb583a9b3dd85f
Justin Tadlock <justin@justintadlock.com>
2013-09-12 13:06:55 -0500

    Version bump to 0.1.2.

--- functions.php
+++ functions.php
@@ -20,7 +20,7 @@
  *
  * @package    SociallyAwkward
  * @subpackage Functions
- * @version    0.1.1
+ * @version    0.1.2
  * @since      0.1.0
  * @author     Justin Tadlock <justin@justintadlock.com>
  * @copyright  Copyright (c) 2013, Justin Tadlock
