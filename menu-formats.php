9527362a3e2a3c9350816a9bd26db81650783120
Justin Tadlock <justin@justintadlock.com>
2014-05-06 09:53:11 -0500

    Drop use of old plural post format string function.

--- menu-formats.php
+++ menu-formats.php
@@ -33,7 +33,7 @@
 
 			foreach ( $formats as $format ) { ?>
 				<li class="menu-item-<?php echo esc_attr( $format ); ?> <?php echo is_tax( 'post_format', "post-format-{$format}" ) ? 'current-menu-item' : ''; ?>">
-				<a href="<?php echo get_post_format_link( $format ); ?>" title="<?php echo esc_attr( hybrid_get_plural_post_format_string( $format ) ); ?>">
+				<a href="<?php echo get_post_format_link( $format ); ?>">
 					<span class="screen-reader-text"><?php echo get_post_format_string( $format ); ?></span>
 				</a>
 				</li>
