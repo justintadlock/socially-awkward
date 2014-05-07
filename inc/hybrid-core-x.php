ad3f264f52a4078e23d39f25816cd6982bbad695
Justin Tadlock <justin@justintadlock.com>
2014-05-07 14:16:06 -0500

    Remove content template filter, which is now handled in HC 2.0.

--- inc/hybrid-core-x.php
+++ inc/hybrid-core-x.php
@@ -10,12 +10,6 @@
  * @since   0.1.0
  */
 
-/* Get the theme prefix. */
-$prefix = hybrid_get_prefix();
-
-/* Filter the content template. */
-add_filter( "{$prefix}_content_template", 'socially_awkward_content_template' );
-
 /* Filter the prev/next posts link attributes. */
 add_filter( 'previous_posts_link_attributes', 'socially_awkward_previous_posts_link_attributes' );
 add_filter( 'next_posts_link_attributes',     'socially_awkward_next_posts_link_attributes' );
@@ -25,30 +19,10 @@ add_filter( 'previous_comments_link_attributes', 'socially_awkward_previous_comm
 add_filter( 'next_comments_link_attributes',     'socially_awkward_next_comments_link_attributes' );
 
 /**
- * Allows for content template based off the attachment mime type (content-attachment-image.php, 
- * content-attachment-audio.php, etc.).
- *
- * @since  0.1.0
- * @access public
- * @param  string  $template
- * @return string
+ * @since      0.1.0
+ * @deprecated 1.0.0
  */
 function socially_awkward_content_template( $template ) {
-
-	if ( 'attachment' === get_post_type() ) {
-
-		$mime_type = get_post_mime_type();
-
-		if ( false !== strpos( $mime_type, '/' ) )
-			list( $type, $subtype ) = explode( '/', $mime_type );
-		else
-			$type = $mime_type;
-
-		$has_template = locate_template( "content-attachment-{$type}.php", false, false );
-
-		return $has_template ? $has_template : $template;
-	}
-
 	return $template;
 }
 
@@ -99,5 +73,3 @@ function socially_awkward_previous_comments_link_attributes( $attributes ) {
 function socially_awkward_next_comments_link_attributes( $attributes ) {
 	return $attributes . ' rel="next"';
 }
-
-?>
