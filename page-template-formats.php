2d9cea48e5c0ce3661cf5e1ef73326551c08bc24
Justin Tadlock <justin@justintadlock.com>
2014-05-06 19:36:02 -0500

    Bring Formats page template up to date with rest of theme code.

--- page-template-formats.php
+++ page-template-formats.php
@@ -5,41 +5,41 @@
 
 get_header(); // Loads the header.php template. ?>
 
-	<div id="content" class="hfeed">
+<main <?php hybrid_attr( 'content' ); ?>>
 
-		<?php if ( have_posts() ) { ?>
+	<?php if ( have_posts() ) : // Checks if any posts were found. ?>
 
-			<?php while ( have_posts() ) { ?>
+		<?php while ( have_posts() ) : // Begins the loop through found posts. ?>
 
-				<?php the_post(); ?>
+			<?php the_post(); // Loads the post data. ?>
 
-				<?php if ( hybrid_post_has_content() ) { ?>
+			<?php if ( hybrid_post_has_content() ) : // Check if the page has content. ?>
 
-					<article<?php hybrid_post_attributes(); ?>>
+				<article <?php hybrid_attr( 'post' ); ?>>
 
-						<header class="entry-header">
-							<h1 class="entry-title"><?php single_post_title(); ?></h1>
-						</header><!-- .entry-header -->
+					<header class="entry-header">
+						<h1 <?php hybrid_attr( 'entry-title' ); ?>><?php single_post_title(); ?></h1>
+					</header><!-- .entry-header -->
 
-						<div class="entry-content">
-							<?php the_content(); ?>
-							<?php wp_link_pages( array( 'before' => '<p class="page-links">' . '<span class="before">' . __( 'Pages:', 'socially-awkward' ) . '</span>', 'after' => '</p>' ) ); ?>
-						</div><!-- .entry-content -->
+					<div <?php hybrid_attr( 'entry-content' ); ?>>
+						<?php the_content(); ?>
+						<?php wp_link_pages(); ?>
+					</div><!-- .entry-content -->
 
-						<?php get_template_part( 'menu', 'formats' ); // Loads the menu-formats.php template ?>
+					<?php hybrid_get_menu( 'formats' ); // Loads the menu/formats.php template ?>
 
-					</article><!-- .hentry -->
+				</article><!-- .entry -->
 
-				<?php } else { ?>
+			<?php else : // If the page doesn't have content. ?>
 
-					<?php get_template_part( 'menu', 'formats' ); // Loads the menu-formats.php template ?>
+				<?php hybrid_get_menu( 'formats' ); // Loads the menu/formats.php template ?>
 
-				<?php } // endif ?>
+			<?php endif; // End check for page content. ?>
 
-			<?php } // endwhile ?>
+		<?php endwhile; // End found posts loop. ?>
 
-		<?php } // endif ?>
+	<?php endif; // End check for posts. ?>
 
-	</div><!-- #content -->
+</main><!-- #content -->
 
 <?php get_footer(); // Loads the footer.php template. ?>
