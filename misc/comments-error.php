890c1a8db65b3c8d677e028703e733e5710049be
Justin Tadlock <justin@justintadlock.com>
2014-05-07 21:55:47 -0500

    Update coding style for misc. comments templates.

--- misc/comments-error.php
+++ misc/comments-error.php
@@ -1,13 +1,16 @@
-<?php if ( pings_open() && !comments_open() ) { ?>
+<?php if ( pings_open() && !comments_open() ) : ?>
 
 	<p class="comments-closed pings-open">
-		<?php printf( __( 'Comments are closed, but <a href="%s" title="Trackback URL for this post">trackbacks</a> and pingbacks are open.', 'socially-awkward' ), esc_url( get_trackback_url() ) ); ?>
+		<?php
+			/* Translators: The two %s are placeholders for HTML. The order can't be changed. */
+			printf( __( 'Comments are closed, but %strackbacks%s and pingbacks are open.', 'socially-awkward' ), '<a href="' . esc_url( get_trackback_url() ) . '">', '</a>' );
+		?>
 	</p><!-- .comments-closed .pings-open -->
 
-<?php } elseif ( !comments_open() ) { ?>
+<?php elseif ( !comments_open() ) : ?>
 
 	<p class="comments-closed">
 		<?php _e( 'Comments are closed.', 'socially-awkward' ); ?>
 	</p><!-- .comments-closed -->
 
-<?php } ?>
+<?php endif; ?>
--- misc/comments-nav.php
+++ misc/comments-nav.php
@@ -1,9 +1,15 @@
-<?php if ( get_option( 'page_comments' ) && 1 < get_comment_pages_count() ) { ?>
+<?php if ( get_option( 'page_comments' ) && 1 < get_comment_pages_count() ) : // Check for paged comments. ?>
 
 	<div class="comments-nav">
 		<?php previous_comments_link( __( '<span class="meta-nav">&larr;</span> Previous', 'socially-awkward' ) ); ?>
-		<span class="page-numbers"><?php printf( __( 'Page %1$s of %2$s', 'socially-awkward' ), ( get_query_var( 'cpage' ) ? absint( get_query_var( 'cpage' ) ) : 1 ), get_comment_pages_count() ); ?></span>
+
+		<span class="page-numbers"><?php 
+			/* Translators: Comments page numbers. 1 is current page and 2 is total pages. */
+			printf( __( 'Page %1$s of %2$s', 'stargazer' ), get_query_var( 'cpage' ) ? absint( get_query_var( 'cpage' ) ) : 1, get_comment_pages_count() ); 
+		?></span>
+
 		<?php next_comments_link( __( 'Next <span class="meta-nav">&rarr;</span>', 'socially-awkward' ) ); ?>
+
 	</div><!-- .comments-nav -->
 
-<?php } ?>
+<?php endif; // End check for paged comments. ?>
