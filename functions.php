20042caf065cd7fa8b6f568db2fc21e35114b902
Justin Tadlock <justin@justintadlock.com>
2014-05-07 19:07:11 -0500

    Add fun quote to functions.php.

--- functions.php
+++ functions.php
@@ -1,5 +1,8 @@
 <?php
 /**
+ * "You know, everyone’s a Democrat until they get a little money. Then they come to their senses." 
+ * ~ Harold Weir (Freaks and Geeks)
+ *
  * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
  * General Public License as published by the Free Software Foundation; either version 2 of the License, 
  * or (at your option) any later version.
